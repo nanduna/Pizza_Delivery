6c946705e9f761ba30f02b6b3461b754c6a05bc1
Nandun Abeykoon <67328995+nanduna@users.noreply.github.com>
2024-02-19 14:10:15 +0530

    Changes

--- client/src/Component/Pizza.js
+++ client/src/Component/Pizza.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState } from "react";
 
 export default function Pizza({ pizza }) {
+  const [quantity, setquantity] = useState(1);
+  const [varient, setvarient] = useState("small");
+
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h1>{pizza.name}</h1>
-      <img src={pizza.image} className='img-fluid' style={{height :'200px', width : '200px'}}/>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src={pizza.image}
+          className="img-fluid"
+          style={{ height: "200px", width: "200px" }}
+        />
+      </div>
+
+      <div className="flex-container">
+        <div className="w-100">
+          <p>Variants</p>
+          <select
+            value={varient}
+            onChange={(e) => {
+              setvarient(e.target.value);
+            }}
+          >
+            {pizza.varients.map((varient) => {
+              return <option value={varient}>{varient}</option>;
+            })}
+          </select>
+        </div>
+        <div className="w-100">
+          <p>Quantity</p>
+          <select
+            value={quantity}
+            onChange={(e) => {
+              setquantity(e.target.value);
+            }}
+          >
+            {[...Array(10).keys()].map((x, i) => {
+              return <option value={i + 1}>{i + 1}</option>;
+            })}
+          </select>
+        </div>
+      </div>
+      <div className="flex-container">
+        <div className="w-100">
+          <h1>Price : {pizza.price[0][varient] * quantity}</h1>
+        </div>
+        <div className="w-100">
+          <button className="btn">
+            Add to cart
+          </button >
+        </div>
+      </div>
     </div>
   );
 }
-
